e8087c38b4f51528665ed19f569bed38ce7113b8
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-05-04 14:29:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
 
-def turn_count(board)
+def turn_count
   turns = 0
   board.each do |board_slot|
     if board_slot == "O" || board_slot == "X"
